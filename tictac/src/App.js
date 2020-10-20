d7dacf334cacd1897d2b07d448cc6ab8786cf082
Keagan <esorts.k990@gmail.com>
2020-10-20 13:33:06 -0400

    first commit

---
+++ tictac/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Board from "./components/Board";
+import Game from "./components/Game";
+import Square from "./components/Square";
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+ <Board />
+    </div>
+  );
+}
+
+export default App;

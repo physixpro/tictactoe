e045f1c69c7a5a15bd2b040f889ac20d32474bbd
Keagan <esorts.k990@gmail.com>
2020-10-21 12:32:32 -0400

    finished game

--- tictac/src/components/Square.js
+++ tictac/src/components/Square.js
@@ -1,17 +1,14 @@
 import React, {Component} from "react";
 
-
-class Square extends Component{
-    constructor(props) { super(props); this.state = {
-        value: null, }; }
-    render(){
-        return(
-            <button className="square" onClick={()=>
-                this.setState({value: 'X'})} >
-                     {this.state.value}
+function Square(props){
+    return(
+        <button className="square" onClick=
+        {props.onClick}>
+            {props.value}
             </button>
-        );
-    }
+    )
 }
+
+
 
 export default Square;
